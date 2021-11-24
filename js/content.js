22b68db8978ccb757a1c9cee33cdf1b2d6e7e941
HuynhEmCuong <huynhcuongem7597@gmail.com>
2021-11-24 13:41:16 +0700

    #fix html

--- js/content.js
+++ js/content.js
@@ -3,6 +3,6 @@ chrome.runtime.sendMessage({todo:"ShowPage"})
 chrome.runtime.onMessage.addListener(function (request,render, sendResponse) {
     if(request.todo =="addValue"){
         var valueAdd = "HCE" + request.valueChange;
-        $("#user_interface").text(valueAdd)
+        $("#stable_apis").text(valueAdd)
     }
 })
