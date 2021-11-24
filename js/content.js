f8c8bca6a271eaf4b7f665f0da468b98b2b863cf
HuynhEmCuong <huynhcuongem7597@gmail.com>
2021-11-24 15:14:56 +0700

    finish

--- js/content.js
+++ js/content.js
@@ -2,7 +2,25 @@ chrome.runtime.sendMessage({todo:"ShowPage"})
 
 chrome.runtime.onMessage.addListener(function (request,render, sendResponse) {
     if(request.todo =="addValue"){
-        var valueAdd = "HCE" + request.valueChange;
-        $("#stable_apis").text(valueAdd)
+        var valueAdd =  request.valueChange;
+
+         $("tr.notSunday ").each(function (index,elem){
+           var date = toDate($(this).find("td>span").text());
+           console.log(date.getDay())
+
+           if (date.getDay() <6){
+        
+                $(this).find("td >textarea").text(valueAdd)
+                $(this).find("td:nth-child(3) > input").val("8.00")
+                if(valueAdd =="")
+                    $(this).find("td:nth-child(3) > input").val("0.00")
+                
+           }
+         });
     }
 })
+
+function toDate(dateStr) {
+    var parts = dateStr.split("/")
+    return new Date(parts[2], parts[1] - 1, parts[0])
+  }
--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,22 @@
 $(function (){
 
-  var color = $("#fontColor").val();
-$("#fontColor").on("Change paste keyup",function () {
+  var color = $("#value").val();
+$("#value").on("Change paste keyup",function () {
     color = $(this).val();
 })
 
   $("#btn_submit").click(function () {
-    
       chrome.tabs.query({active:true, currentWindow:true},function (tabs) {
           chrome.tabs.sendMessage(tabs[0].id,{todo:"addValue", valueChange:color})
       })
   })
+
+  $("#btn_delete").click(function () {
+    color ="";
+    $("#value").val("");
+    chrome.tabs.query({active:true, currentWindow:true},function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id,{todo:"addValue", valueChange:color})
+    })
+})
+  
 })
